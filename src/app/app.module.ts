a51b6122890f83487f6eb85a5fd222d066192ae1
OJAS-IT\chrakesh <rakesh.chengelli@ojas-it.com>
2018-06-20 18:05:32 +0530

    Added all files into rep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes,RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { SampleComponent } from './sample/sample.component';
+import { SubuserComponent } from './user/subuser/subuser.component';
+import { SubadminComponent } from './admin/subadmin/subadmin.component';
 
+const routes: Routes=[{
+    path:"user",
+    component: UserComponent,
+    children:[{
+      path:"subuser",
+      component:SubuserComponent
+    }]
+},
+{
+  path:"admin",
+  component:AdminComponent,
+  children:[{
+    path:"subadmin",
+    component:SubadminComponent
+  }]
+},
+{
+  path:"sample",
+  component:SampleComponent
+}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AdminComponent,
+    SampleComponent,
+    SubuserComponent,
+    SubadminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/subuser/subuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subuser',
+  templateUrl: './subuser.component.html',
+  styleUrls: ['./subuser.component.css']
+})
+export class SubuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
